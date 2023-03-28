d4101f4662f168c4bde161d8aceaf9ed2dc62c70
Carlosdp2023 <ing.cpmoreno@gmail.com>
2023-03-28 10:31:20 -0500

    Solución primer punto

--- p1.py
+++ p1.py
@@ -1,1 +1,9 @@
-conversion.py
+#Conversión de temperaturas.
+c = float(input('Ingrese la temperatura en grados Celcius: '))
+f = ((9/5)*c) + 32
+r = ((c*1.8) + 491.67)
+k = c + 273.15
+
+print(c, 'es equuivalente a',f,'grados fahrenheit')
+print(c, 'es equuivalente a',r,'grados Rankine')
+print(c, 'es equuivalente a',k,'grados Kelvin')
